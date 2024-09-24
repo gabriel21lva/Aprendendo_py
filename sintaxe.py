495048b78c722552a6354e89996ec2f804fddebc
gabriel21lva <gabrielsilvaesousa@hotmail.com>
2024-09-24 17:50:13 -0300

    Aprendendo_Python

---
+++ sintaxe.py
@@ -0,0 +1,19 @@
+print("Calculadora Simples:")
+
+N1 = int(input("Digite um numero:"))
+N2 = int(input("Digite outro numero:"))
+
+print("1- adiçao")
+print("2- subtraçao")
+print("3- multipliçao")
+print("4- divisao")
+
+operacao = int(input("Qual operaçao deseja fazer?"))
+
+
+
+         
+
+
+
+
